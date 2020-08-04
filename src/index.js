22979c4d46ff4b4ee0cbfe8e49f82f2c4c633254
Higor <higorpereirabarbosa@gmail.com>
2020-08-04 10:26:38 -0300

    pausado no ep5

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 
 import App from './pages/home/App';
 import Video from './pages/cadastro/Video'
+import Categorias from './pages/cadastro/Categoria'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={App} exact></Route>
-      <Route path='/cadstros/video' exact component={Video} ></Route>
+      <Route path='/cadastros/video' exact component={Video} ></Route>
+      <Route path='/cadastros/categorias' exact component={Categorias}></Route>
       <Route  component={() => (<div>404</div>)}></Route>
     </Switch>
   </BrowserRouter>
